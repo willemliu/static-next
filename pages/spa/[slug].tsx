c416269e51bb7a0a5bff7bdb6d7a19b08bcac26a
Willem Liu <willem_liu@hotmail.com>
2020-02-26 16:22:02 +0100

    slug

--- pages/spa/[slug].tsx
+++ pages/spa/[slug].tsx
@@ -26,8 +26,8 @@ Index.getInitialProps = async (context) => {
     const res = await fetch(
         "https://static-next.willemliu.now.sh/api/test"
     ).then((res) => res.json());
-    console.log("getServerProps", res, context.params, context.query);
-    return { ...res, name: context.params.slug };
+    console.log("getServerProps", res, context.query);
+    return { ...res, name: context.query.slug };
 };
 
 export default Index;
