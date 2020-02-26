9ba9045427f98a19b5de91c0888af508fb7eb8ac
Willem Liu <willem_liu@hotmail.com>
2020-02-26 16:32:39 +0100

    Changed text

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ function Index(props: any) {
         <>
             <Menu />
             <h1>Test {props?.name}</h1>
-            {props?.date && <p>Static site generation: {props?.date}</p>}
+            {props?.date && <p>Site generated on: {props?.date}</p>}
             <div>
                 <a
                     href="https://api.zeit.co/v1/integrations/deploy/QmNYzrk6Y9P1qL6PWVTnp9fForR7FuZp8JTfFyZ71CnVVS/Z7ZOSYG4EM"
