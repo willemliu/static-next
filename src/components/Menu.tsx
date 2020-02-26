ff46781ae2ceb55e061c0751415b5e2f6b6d1ade
Willem Liu <willem_liu@hotmail.com>
2020-02-26 16:09:24 +0100

    Refactored endpoints

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -8,10 +8,10 @@ function Menu() {
             <Link href="/">
                 <a>Home</a>
             </Link>
-            <Link href="/test/[slug]" as="/test/hello-world">
+            <Link href="/ssg/[slug]" as="/ssg/hello-world">
                 <a>Static</a>
             </Link>
-            <Link href="/test/[slug]" as="/test/hello-world2">
+            <Link href="/ssg/[slug]" as="/ssg/hello-world2">
                 <a>Static 2</a>
             </Link>
             <Link href="/ssr/[slug]" as="/ssr/hello-world">
