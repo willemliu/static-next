9f6f68bb0db5d8b9ecdd7e47f1348b2d3026ba0d
Willem Liu <willem_liu@hotmail.com>
2020-03-10 12:32:13 +0100

    Added spinner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Router from "next/router";
 import { Menu } from "../src/components/Menu";
 import { Footer } from "../src/components/Footer";
+import { Spinner } from "../src/components/Spinner";
+
 export default function App({ Component, pageProps }) {
+    const [loading, setLoading] = useState(false);
+
+    Router.events.on("routeChangeStart", () => {
+        setLoading(true);
+    });
+    Router.events.on("routeChangeComplete", () => {
+        setLoading(false);
+    });
+
     return (
         <>
             <Menu />
             <Component {...pageProps} />
             <Footer />
+            {loading && <Spinner />}
         </>
     );
 }
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Spinner.module.scss";
+
+function Spinner() {
+    return (
+        <section className={styles.container}>
+            <div className={styles.ellipsis}>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </section>
+    );
+}
+
+export { Spinner };
