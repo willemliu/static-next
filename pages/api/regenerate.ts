27be35c73fd23caef049de9649348ee2682b0d3a
Willem Liu <willem.liu@fdmediagroep.nl>
2022-01-06 11:05:45 +0100

    Added AWS Amplify webhook regenerate

--- pages/api/regenerate.ts
+++ pages/api/regenerate.ts
@@ -1,13 +1,20 @@
-import Cors from "micro-cors";
-import "isomorphic-unfetch";
-const cors = Cors({ allowMethods: ["GET", "HEAD"] });
+import Cors from 'micro-cors';
+import 'isomorphic-unfetch';
+const cors = Cors({ allowMethods: ['GET', 'HEAD'] });
 
 async function regenerate(req, res) {
     res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    const response = await fetch(
-        "https://api.zeit.co/v1/integrations/deploy/QmNYzrk6Y9P1qL6PWVTnp9fForR7FuZp8JTfFyZ71CnVVS/3qWMRbPwMg"
+    res.setHeader('Content-Type', 'application/json');
+    const webhookVercel = fetch(
+        'https://api.vercel.com/v1/integrations/deploy/QmNYzrk6Y9P1qL6PWVTnp9fForR7FuZp8JTfFyZ71CnVVS/3qWMRbPwMg'
     ).then((res) => res.json());
+
+    const webhookAWSAmplify = fetch(
+        'https://webhooks.amplify.eu-west-1.amazonaws.com/prod/webhooks?id=39136c3c-a922-4014-8f44-7ffc09af7da6&token=hCtGi1YQdYDPvBJPlSFU9O6s8XrMMo7hjuLJDdz5wn4'
+    ).then((res) => res.json());
+
+    const response = await Promise.all([webhookVercel, webhookAWSAmplify]);
+
     res.end(JSON.stringify(response));
 }
 
