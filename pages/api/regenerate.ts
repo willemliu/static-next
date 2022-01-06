994c227e24fd39f2987fab09438e7b77dcf996c1
Willem Liu <willem.liu@fdmediagroep.nl>
2022-01-06 11:15:16 +0100

    Using env for webhook

--- pages/api/regenerate.ts
+++ pages/api/regenerate.ts
@@ -5,14 +5,13 @@ const cors = Cors({ allowMethods: ['GET', 'HEAD'] });
 async function regenerate(req, res) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    const webhookVercel = fetch(
-        'https://api.vercel.com/v1/integrations/deploy/QmNYzrk6Y9P1qL6PWVTnp9fForR7FuZp8JTfFyZ71CnVVS/3qWMRbPwMg'
-    ).then((res) => res.json());
+    const webhookVercel = fetch(process.env.WEBHOOK_VERCEL).then((res) =>
+        res.json()
+    );
 
-    const webhookAWSAmplify = fetch(
-        'https://webhooks.amplify.eu-west-1.amazonaws.com/prod/webhooks?id=39136c3c-a922-4014-8f44-7ffc09af7da6&token=hCtGi1YQdYDPvBJPlSFU9O6s8XrMMo7hjuLJDdz5wn4',
-        { method: 'POST' }
-    ).then((res) => res.json());
+    const webhookAWSAmplify = fetch(process.env.WEBHOOK_AWS_AMPLIFY, {
+        method: 'POST',
+    }).then((res) => res.json());
 
     const response = await Promise.all([webhookVercel, webhookAWSAmplify]);
 
